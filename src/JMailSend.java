c534faf1c27b421a4c8ef548b7a3fc3b0a6233f9
Rodrigo Santos <rodrsouza@gmail.com>
2015-05-04 23:18:23 -0300

    Implemented the JMailSend class.

--- src/JMailSend.java
+++ src/JMailSend.java
@@ -17,4 +17,49 @@ public class JMailSend
 		subject_ = "";
 		content_ = "";
 	}
+	
+	public final ArrayList<String> getTo()
+	{
+		return to_;
+	}
+	
+	public void addTo(final String to)
+	{
+		to_.add(to);
+	}
+	
+	public void addAttachment(final String attachment)
+	{
+		attachment_list_.add(attachment);
+	}
+	
+	public void setSubject(final String subject)
+	{
+		subject_ = subject;
+	}
+	
+	public final void setContent(final String content)
+	{
+		content_ = content;
+	}
+	
+	public final String getContent()
+	{
+		return content_;
+	}
+	
+	public final ArrayList<String> getAttachments()
+	{
+		return attachment_list_;
+	}
+	
+	public final String getFrom()
+	{
+		return from_;
+	}
+	
+	public final String getSubject()
+	{
+		return subject_;
+	}
 }
