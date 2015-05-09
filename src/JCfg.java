65fa7f192e6fbf88f105aec69dbec665146e4519
Rodrigo Santos <rodrsouza@gmail.com>
2015-05-08 23:26:51 -0300

    Added smtp to config file.

--- src/JCfg.java
+++ src/JCfg.java
@@ -11,8 +11,22 @@ public final class JCfg extends JPropertiesFile
 	
 	private final String imap_server_key_ = "imap_server";
 	private String imap_server_ = "imap.server.com";
+	
+	private final String smtp_server_key_ = "smtp_server";
+	private String smtp_server_ = "smtp.server.com";
+	
+	private final String smtp_port_key_ = "smtp_port";
+	private String smtp_port_ = "465";
+	
+	private final String smtp_ssl_key_ = "smtp_ssl";
+	private String smtp_ssl_ = "true";
+	
+	private final String smtp_need_auth_key_ = "smtp_need_auth";
+	private String smtp_need_auth_ = "true";
+	
 	private final String user_name_key_ = "username";
 	private String user_name_ = "usr";
+	
 	private final String password_key_ = "password";
 	private String password_ = "pwd";
 	
@@ -39,6 +53,10 @@ public final class JCfg extends JPropertiesFile
 		props_ = load();
 		
 		imap_server_ = props_.getProperty(imap_server_key_);
+		smtp_server_ = props_.getProperty(smtp_server_key_);
+		smtp_port_ = props_.getProperty(smtp_port_key_);
+		smtp_ssl_ = props_.getProperty(smtp_ssl_key_);
+		smtp_need_auth_ = props_.getProperty(smtp_need_auth_key_);
 		user_name_ = props_.getProperty(user_name_key_);
 		password_ = props_.getProperty(password_key_);
 	}
@@ -48,6 +66,10 @@ public final class JCfg extends JPropertiesFile
 		props_ = new Properties();
 		
 		props_.setProperty(imap_server_key_, imap_server_);
+		props_.setProperty(smtp_server_key_, smtp_server_);
+		props_.setProperty(smtp_port_key_, smtp_port_);
+		props_.setProperty(smtp_ssl_key_, smtp_ssl_);
+		props_.setProperty(smtp_need_auth_key_, smtp_need_auth_);
 		props_.setProperty(user_name_key_, user_name_);
 		props_.setProperty(password_key_, password_);
 		
@@ -67,5 +89,25 @@ public final class JCfg extends JPropertiesFile
 	public final String getImapServer()
 	{
 		return imap_server_;
+	}
+	
+	public final String getSmtpServer()
+	{
+		return smtp_server_;
+	}
+	
+	public final String getSmtpPort()
+	{
+		return smtp_port_;
+	}
+	
+	public final String getSmtpSSL()
+	{
+		return smtp_ssl_;
+	}
+	
+	public final String getSmtpNeedAuth()
+	{
+		return smtp_need_auth_;
 	}
 }
